583a33e4c65e53663ea314699597024e96f37786
Gasserashraf <gasser.ashraf977@gmail.com>
2020-11-02 23:08:50 +0200

    order page is done

--- lib/src/pages/order_page.dart
+++ lib/src/pages/order_page.dart
@@ -9,13 +9,144 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      padding: EdgeInsets.symmetric(horizontal: 10.0),
-      children: <Widget>[
-        OrderCard()
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          "Your Food Cart",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              OrderCard(),
+              OrderCard(),
 
+            ],
+          ),
+          _buildTotalContainer(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTotalContainer() {
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Cart total",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Text(
+                "23.0",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Discount",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Text(
+                "3.0",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Tax",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Text(
+                "0.5",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+          Divider(
+            height: 40.0,
+            color: Color(0xffd3d3d3),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Sub Total",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Text(
+                "26.5",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 50.0,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(30.0)),
+            child: Center(
+              child: Text(
+                "Proceed to Checkout",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/src/widgets/order_card.dart
+++ lib/src/widgets/order_card.dart
@@ -3,60 +3,121 @@ import 'package:flutter/material.dart';
 class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-          child: Row(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xffd3d3d3), width: 2.0),
-                    borderRadius: BorderRadius.circular(10.0)),
-                height: 75.0,
-                width: 45.0,
-                child: Column(
-                  children: <Widget>[
-                    InkWell(
-                      child: Icon(Icons.keyboard_arrow_up,
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffd3d3d3), width: 2.0),
+                  borderRadius: BorderRadius.circular(10.0)),
+              height: 75.0,
+              width: 45.0,
+              child: Column(
+                children: <Widget>[
+                  InkWell(
+                    child:
+                        Icon(Icons.keyboard_arrow_up, color: Color(0xffd3d3d3)),
+                    onTap: () {
+                      print('up');
+                    },
+                  ),
+                  Text(
+                    '0',
+                    style: TextStyle(fontSize: 18.0, color: Color(0xffd3d3d3)),
+                  ),
+                  InkWell(
+                      child: Icon(Icons.keyboard_arrow_down,
                           color: Color(0xffd3d3d3)),
                       onTap: () {
-                        print('up');
-                      },
-                    ),
-                    Text(
-                      '0',
-                      style:
-                          TextStyle(fontSize: 18.0, color: Color(0xffd3d3d3)),
-                    ),
-                    InkWell(
-                        child: Icon(Icons.keyboard_arrow_down,
-                            color: Color(0xffd3d3d3)),
-                        onTap: () {
-                          print('down');
-                        }),
-                  ],
+                        print('down');
+                      }),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Container(
+              width: 70.0,
+              height: 70.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/lunch.jpeg"),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(35.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 5.0,
+                        offset: Offset(0.0, 5.0))
+                  ]),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Grilled Chiken",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
                 ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "3.0",
+                  style: TextStyle(color: Colors.orangeAccent, fontSize: 18.0),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Container(
+                  width: 120.0,
+                  height: 25.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              "Chicken",
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            InkWell(
+                              child: Text("x",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold)),
+                              onTap: () {},
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Spacer(),
+            GestureDetector(
+              child: Icon(
+                Icons.cancel,
+                color: Colors.grey,
               ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Container(
-                width: 70.0,
-                height: 70.0,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/images/lunch.jpeg"),
-                        fit: BoxFit.cover),
-                    borderRadius: BorderRadius.circular(35.0),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 5.0,
-                      offset: Offset(0.0, 5.0)
-                    )]),
-              ),
-            ],
-          ),
+              onTap: () {},
+            )
+          ],
         ),
       ),
     );
